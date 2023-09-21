4bffb7715ece9481dd209e9af2829383b2ad1662
Marc0E <mestradamonge@cnm.edu>
2023-09-21 14:50:48 -0600

    Classic switch solution.

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -4,9 +4,26 @@ public class Main {
 
   //psvm to create this
   public static void main(String[] args) {
-    for(int i = 1; i <= 100; i++)
-    {
-      boolean divisibleBy3 = (i % 3 == 0);
+    for (int i = 1; i <= 100; i++) {
+      int remainder = i % 15;
+      switch (remainder) {
+        case 0:
+          System.out.println("fizzbuzz");
+          break;
+        case 3:
+        case 6:
+        case 9:
+        case 12:
+          System.out.println("fizz");
+          break;
+        case 5:
+        case 10:
+          System.out.println("buzz");
+          break;
+        default:
+          System.out.println(i);
+      }
+/*      boolean divisibleBy3 = (i % 3 == 0);
       boolean divisibleBy5 = (i % 5 == 0);
       if(divisibleBy3 || divisibleBy5){
         if(divisibleBy3) {
@@ -19,7 +36,7 @@ public class Main {
       else {
         System.out.print(i);
       }
-      System.out.println();
+      System.out.println(); */
 
     }
   }
