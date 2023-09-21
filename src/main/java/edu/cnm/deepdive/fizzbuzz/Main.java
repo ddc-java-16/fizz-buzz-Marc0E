f27c77b4df5d8d6ac63637b0b0804b9258f7c8a6
Marc0E <mestradamonge@cnm.edu>
2023-09-21 16:11:51 -0600

    Added MainTest

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -2,6 +2,9 @@ package edu.cnm.deepdive.fizzbuzz;
 
 public class Main {
 
+  private static final String FIZZ_RESULT = "fizz";
+  private static final String BUZZ_RESULT = "buzz";
+  private static final String FIZZ_BUZZ_RESULT = FIZZ_RESULT+BUZZ_RESULT;
   //psvm to create this
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
@@ -26,12 +29,13 @@ public class Main {
     }
   }
 
-  private static String detectFizzBuzz(int i) {
+
+   static String detectFizzBuzz(int i) {
     int remainder = i % 15;
     return switch (remainder) {
-      case 0 -> ("fizzbuzz");
-      case 3, 6, 9, 12 -> ("fizz");
-      case 5, 10 -> ("buzz");
+      case 0 -> FIZZ_BUZZ_RESULT;
+      case 3, 6, 9, 12 -> FIZZ_RESULT;
+      case 5, 10 -> BUZZ_RESULT;
       default -> //doesn't have to be the last
           String.valueOf(i);
     };
