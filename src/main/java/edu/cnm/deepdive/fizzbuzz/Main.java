6ff9f73216f217be9c3d07406e54523a64b287d9
Marc0E <mestradamonge@cnm.edu>
2023-10-10 10:09:38 -0600

    Modified to use enum and EnumSet.

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -1,16 +1,21 @@
 package edu.cnm.deepdive.fizzbuzz;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public class Main {
 
   private static final String FIZZ_RESULT = "fizz";
   private static final String BUZZ_RESULT = "buzz";
-  private static final String FIZZ_BUZZ_RESULT = FIZZ_RESULT+BUZZ_RESULT;
+  private static final String FIZZ_BUZZ_RESULT = FIZZ_RESULT + BUZZ_RESULT;
+
   //psvm to create this
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
       //String value = detectFizzBuzz(i); other option to do the same as below
       //System.out.println(value); other option to do the same as below
-      System.out.println(detectFizzBuzz(i));
+      Set<FizzBuzz> value = FizzBuzz.valueOf(i);
+      System.out.println(value.isEmpty() ? i : value );
 /*      boolean divisibleBy3 = (i % 3 == 0);
       boolean divisibleBy5 = (i % 5 == 0);
       if(divisibleBy3 || divisibleBy5){
@@ -29,16 +34,27 @@ public class Main {
     }
   }
 
+  public enum FizzBuzz {
+    FIZZ, BUZZ;
 
-   static String detectFizzBuzz(int i) {
-    int remainder = i % 15;
-    return switch (remainder) {
-      case 0 -> FIZZ_BUZZ_RESULT;
-      case 3, 6, 9, 12 -> FIZZ_RESULT;
-      case 5, 10 -> BUZZ_RESULT;
-      default -> //doesn't have to be the last
-          String.valueOf(i);
-    };
+    public static Set<FizzBuzz> valueOf(int value) {
+
+      Set<FizzBuzz> result = EnumSet.noneOf(FizzBuzz.class);
+      if(value % 3 == 0) {
+        result.add(FIZZ);
+      }
+      if(value % 5 == 0 ){
+        result.add(BUZZ);
+      }
+      return result;
+//    int remainder = i % 15;
+//    return switch (remainder) {
+//      case 0 -> FIZZ_BUZZ_RESULT;
+//      case 3, 6, 9, 12 -> FIZZ_RESULT;
+//      case 5, 10 -> BUZZ_RESULT;
+//      default -> //doesn't have to be the last
+//          String.valueOf(i);
+//    };
     /*other way to do it
         String value = switch (remainder) {
       case 0 -> ("fizzbuzz");
@@ -49,6 +65,6 @@ public class Main {
     };
     return value;
      */
+    }
   }
-
 }
